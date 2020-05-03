9a23dac92a4fdec641168a1534920d6d8ed2087f
Durianss <46402330+Durianss@users.noreply.github.com>
2020-05-03 18:11:34 -0500

    Adjusting height

--- src/football/worker/core/player/heightToRating.ts
+++ src/football/worker/core/player/heightToRating.ts
@@ -1,7 +1,7 @@
 import limitRating from "../../../../deion/worker/core/player/limitRating";
 
 const heightToRating = (heightInInches: number) => {
-	const minHgt = 64; // 5'4"
+	const minHgt = 64; // 5'6"
 
 	const maxHgt = 82; // 6'10"
 
